99ae5da7d4c0a61a3c58968182856d7a2fdb0043
hamzabl8r <hamzabeji001@gmail.com>
2025-07-01 16:26:20 +0100

    color box

---
+++ Js/javascript.js
@@ -0,0 +1,10 @@
+document.getElementById('btn').addEventListener('click', function() {
+    document.querySelector('.box').style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
